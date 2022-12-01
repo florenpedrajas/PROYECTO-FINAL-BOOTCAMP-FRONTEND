10ae5bf27f2ee9b8f63b5f0062139fc3e1e4a0a2
albertoncp <alberto.decela@bootcamp-upgrade.com>
2022-12-01 19:16:52 +0100

    GetLocation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import logo from './logo.svg';
+import {getLocation} from "../src/redux/garaje/garaje.functions"
 import './App.css';
 
 function App() {
+  
+  const dispatch = useDispatch();
+
+  
+  
+  useEffect(() => {
+    console.log("estoy dentro");
+    dispatch(getLocation());
+  }, []);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
     </div>
   );
 }
---
+++ src/redux/garaje/garaje.functions.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+export const getLocation = ()=>async(dispatch)=>{
+   console.log("hola");
+    dispatch({type:'gettingLocation'})
+
+    try {
+
+        function getPosition() {
+            // Simple wrapper
+            return new Promise((res, rej) => {
+                navigator.geolocation.getCurrentPosition(res, rej);
+            });
+        }
+        
+        async function main() {
+            var position = await getPosition();  // wait for getPosition to complete
+            console.log(position.coords.latitude);
+        
+            return position
+        }
+        const res = main()
+        
+        dispatch({type: 'getLocation', payload: "hola"})
+
+    } catch (error) {
+        console.log(error);
+        /* dispatch({type:'errorClothes', payload:error.message}) */
+    }
+
+}
